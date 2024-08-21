b192f0309e983b26edf96611c88d02cc02bcaae4
Eden Brawn <edenbrawn@gmail.com>
2024-08-21 16:09:05 +0100

    edited java script

--- script.js
+++ script.js
@@ -3,10 +3,36 @@ function submit(e){/*https://stackoverflow.com/questions/63540236/use-js-to-dete
     var fname = document.getElementById('fname');
     var lname = document.getElementById('lname');
     var emailadd = document.getElementById('emailadd');
-    if(fname.value == ""){
-        alert("Not complete");
-        fname.style.border = '0.01em solid red';
-
+    var generalenquiry = document.querySelector('input[name=enquiryg]');
+    var supportrequest = document.querySelector('input[name=enquirys]');
+    var message = document.getElementById('message');
+    var checkbox = document.querySelector('input[name=checkbox]');
+    if(fname.value == "" || lname.value == "" || emailadd.value == "" ||
+        !generalenquiry ||  !supportrequest ||
+        message.value == "" || !checkbox
+    ){
+        if(fname.value == ""){
+            fname.style.border = '0.01em solid red';
+        }
+        if(lname.value == ""){
+            lname.style.border = '0.01em solid red';
+        }
+        if(emailadd.value == ""){
+            emailadd.style.border = '0.01em solid red';
+        }
+        if(!generalenquiry.value){
+            fieldset.style.border = '0.01em solid red';
+        }
+        if(!supportrequest){
+            fieldset.style.border = '0.01em solid red';  
+        }
+        if(message.value == ""){
+            message.style.border = '0.01em solid red';
+        }
+        if(!checkbox){
+            checkbox.style.border = '0.01em solid red';
+        }
+        
     } else{
         var element = document.getElementById('alert');
         element.style.display = 'block';

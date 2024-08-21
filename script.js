d1736d1e5291090c12531b3b94fc6ea6fdea9eaf
Eden Brawn <edenbrawn@gmail.com>
2024-08-21 15:00:45 +0100

    prevented oage reset on submit

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-function submit(){/*https://stackoverflow.com/questions/63540236/use-js-to-determine-if-all-form-fields-are-filled-and-correctly#:~:text=You%20can%20select%20all%20the,input%20with%20more%20precise%20selector. */
+function submit(e){/*https://stackoverflow.com/questions/63540236/use-js-to-determine-if-all-form-fields-are-filled-and-correctly#:~:text=You%20can%20select%20all%20the,input%20with%20more%20precise%20selector. */
+    e.preventDefault();
     var fname = document.getElementById('fname');
     if(fname.value == ""){
         alert("Not complete");
-        console("worked");
+        var fname = document.getElementsByClassName('firstnameinput');
+        fname.style.border = '0.01em solid red';
 
-    } 
+    } else{
+        var element = document.getElementById('alert');
+        element.style.display = 'block';
+    }
 }
 
 function submitButtonListener(){

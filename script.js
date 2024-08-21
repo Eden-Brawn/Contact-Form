df2af0531d4ca575bd7fed1a56b38a30ae9aae12
Eden Brawn <edenbrawn@gmail.com>
2024-08-21 14:38:10 +0100

    corrected id

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function submit(){/*https://stackoverflow.com/questions/63540236/use-js-to-determine-if-all-form-fields-are-filled-and-correctly#:~:text=You%20can%20select%20all%20the,input%20with%20more%20precise%20selector. */
-    var fname = document.getElementById("firstname");
+    var fname = document.getElementById('fname');
     if(fname.value == ""){
         alert("Not complete");
+        console("worked");
 
     } 
 }
